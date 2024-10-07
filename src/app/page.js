d087f124f03a5ae88de8b9410625c6e62c3bc843
adrianoresouza <adrianoresouza@gmail.com>
2024-10-07 07:08:24 -0300

    Add: Sequelize

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,33 @@
 import ChartOverview from "@/components/chart";
-import { Sales } from "@/components/sales/indesx";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Wallet } from "lucide-react";
-import Image from "next/image";
 
-export default function Home() {
-	return (
-		<main className="sm:ml-14 p-4">
-			<section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-				<Card>
-					<CardHeader>
-						<div className="flex items-center justify-center">
-							<CardTitle className="text-lg sm:text-xl text-gray-600 select-none">
-								Carteira
-							</CardTitle>
-							<Wallet className="ml-auto w-4 h-4"/>
-						</div>
+export default function Home() {	
+return (
+	<main className="sm:ml-14 p-4">
+		<section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+			<Card>
+				<CardHeader>
+					<div className="flex items-center justify-center">
+						<CardTitle className="text-lg sm:text-xl text-gray-600 select-none">
+							Carteira
+						</CardTitle>
+						<Wallet className="ml-auto w-4 h-4" />
+					</div>
 
-						<CardDescription>
-							Total da Carteira
-						</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<p className="text-base sm:text-lg font-bold">R$ 100.000</p>
-					</CardContent>
-				</Card>
-			</section>
+					<CardDescription>
+						Total da Carteira
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p className="text-base sm:text-lg font-bold">R$ 100.000</p>
+				</CardContent>
+			</Card>
+		</section>
 
-			<section className="mt-4 flex flex-col md:flex-row gap-4">
-				<ChartOverview/>
-			</section>
-		</main>
-	);
+		<section className="mt-4 flex flex-col md:flex-row gap-4">
+			<ChartOverview />
+		</section>
+	</main>
+);
 }
